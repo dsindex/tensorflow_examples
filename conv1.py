ed4eb1b7cdbed7bc15bacc62b1f08fc450bd94a4
dsindex <>
2016-04-01 02:26:16 +0000

    -

--- conv1.py
+++ conv1.py
@@ -58,7 +58,7 @@ NUM_THREADS = 5
 sess = tf.Session(config=tf.ConfigProto(intra_op_parallelism_threads=NUM_THREADS))
 init = tf.initialize_all_variables()
 sess.run(init)
-for i in range(20000):
+for i in range(1000):
 	batch_xs, batch_ys = mnist.train.next_batch(50)
 	if i % 100 == 0:
 		print "step : ", i, "training accuracy :", sess.run(accuracy, feed_dict={x: batch_xs, y_: batch_ys, keep_prob: 1.0})	

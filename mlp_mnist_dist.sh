fc3bdee7286d21b09004603c0cbe1d1dd8c40355
dsindex <>
2016-05-02 00:53:56 +0000

    -

--- mlp_mnist_dist.sh
+++ mlp_mnist_dist.sh
@@ -116,12 +116,12 @@ nohup python ${CDIR}/mlp_mnist_dist.py --ps_hosts=localhost:2222,localhost:2223 
 		   --worker_hosts=localhost:2224,localhost:2225 \
 		   --job_name=ps --task_index=1 &
 
-nohup python ${CDIR}/mlp_mnist_dist.py --ps_hosts=localhost:2222,localhost:2223 \
+python ${CDIR}/mlp_mnist_dist.py --ps_hosts=localhost:2222,localhost:2223 \
 		   --worker_hosts=localhost:2224,localhost:2225 \
-		   --job_name=worker --task_index=0 &
-nohup python ${CDIR}/mlp_mnist_dist.py --ps_hosts=localhost:2222,localhost:2223 \
+		   --job_name=worker --task_index=0
+python ${CDIR}/mlp_mnist_dist.py --ps_hosts=localhost:2222,localhost:2223 \
 		   --worker_hosts=localhost:2224,localhost:2225 \
-		   --job_name=worker --task_index=1 &
+		   --job_name=worker --task_index=1
 
 close_fd
 

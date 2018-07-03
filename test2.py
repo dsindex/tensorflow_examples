32aede04729666d5577f25285f9fe86ee5931aa1
dsindex <hazzling@gmail.com>
2018-07-03 11:43:38 +0900

    -

--- test2.py
+++ test2.py
@@ -6,20 +6,13 @@ x = tf.constant([1, 4])
 y = tf.constant([2, 5])
 z = tf.constant([3, 6])
 
-m = tf.Variable([x,y,z])
-mt = tf.convert_to_tensor(m)
-s0 = tf.stack(mt, axis=0)   # pack along first dim
-#s1 = tf.stack(mt, axis=1)   # pack along second dim
+s0 = tf.stack([x,y,z], axis=0)   # pack along first dim
+s1 = tf.stack([x,y,z], axis=1)   # pack along second dim
 
 init_op = tf.global_variables_initializer()
 with tf.Session() as sess:
 	sess.run(init_op)
-	print x
-	print m
-	print sess.run(m)
-	print mt
-	print sess.run(mt)
+	print [x,y,z]
 	print sess.run(s0)
-	print mt
-	#print sess.run(s1)
+	print sess.run(s1)
 
--- test3.py
+++ test3.py
@@ -2,16 +2,25 @@
 
 import tensorflow as tf
 
-x = tf.constant([1, 4])
-y = tf.constant([2, 5])
-z = tf.constant([3, 6])
-
-s0 = tf.stack([x, y, z], axis=0)   # pack along first dim
-s1 = tf.stack([x, y, z], axis=1)   # pack along second dim
+x = tf.constant([1, -4])
+y = tf.constant([ [ [2, 5],
+                    [3, 4], 
+                    [5, 7] ],
+                  [ [1, -1],
+                    [0, 4],
+                    [9, 3] ] ])
 
 init_op = tf.global_variables_initializer()
 with tf.Session() as sess:
 	sess.run(init_op)
-	print sess.run(s0)
-	print sess.run(s1)
+	print sess.run(tf.abs(x))
+	print sess.run(tf.reduce_max(y, reduction_indices=0))
+	print sess.run(tf.reduce_max(y, reduction_indices=1))
+	print sess.run(tf.reduce_max(y, reduction_indices=2))
+	print sess.run(tf.sign(tf.reduce_max(y, reduction_indices=2)))
+	print sess.run(tf.reduce_sum(tf.sign(tf.reduce_max(y, reduction_indices=2)),reduction_indices=1))
+	print sess.run(tf.cast(tf.reduce_sum(tf.sign(tf.reduce_max(y, reduction_indices=2)),reduction_indices=1),tf.int32))
+
+	print sess.run(tf.reduce_sum(y, reduction_indices=1))
+	print sess.run(tf.reduce_sum(y, reduction_indices=0))
 

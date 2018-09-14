68675e0a18c83c703d149f24d2211a839e58a47f
dsindex <hazzling@gmail.com>
2018-09-14 17:50:27 +0900

    -

--- test5.py
+++ test5.py
@@ -8,10 +8,19 @@ x = tf.constant([ [0, 0, -1000, -1000],
                   [0, 0, 0, -1000] ] )
 
 y = tf.expand_dims(x, 0)
+
 z = tf.transpose(y, [1, 0, 2])
-w = tf.expand_dims(z, 1)
+
+k = tf.tile(z, [1, 2, 1])
+print k
+
+w = tf.expand_dims(k, 1)
 print w
-u = tf.tile(w, [1, 1, 4, 1])
+
+j = tf.transpose(w, [0, 2, 1, 3])
+print j
+
+u = tf.tile(j, [1, 1, 4, 1])
 print u
 
 init_op = tf.global_variables_initializer()
@@ -19,5 +28,7 @@ with tf.Session() as sess:
 	sess.run(init_op)
 	print sess.run(y)
 	print sess.run(z)
+	print sess.run(k)
 	print sess.run(w)
+	print sess.run(j)
 	print sess.run(u)

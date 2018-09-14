54ef451bb7095d0df977a8a0b61e26c4ea2afa2a
dsindex <hazzling@gmail.com>
2018-09-14 16:45:11 +0900

    -

--- test5.py
+++ test5.py
@@ -7,10 +7,11 @@ x = tf.constant([ [0, 0, -1000, -1000],
                   [0, 0, 0, 0], 
                   [0, 0, 0, -1000] ] )
 
-y = tf.expand_dims(x, -1)
-z = tf.tile(y, [1, 1, 5])
+y = tf.expand_dims(x, 0)
+z = tf.expand_dims(y, 0)
 
 init_op = tf.global_variables_initializer()
 with tf.Session() as sess:
 	sess.run(init_op)
+	print sess.run(y)
 	print sess.run(z)

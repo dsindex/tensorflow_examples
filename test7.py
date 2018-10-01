2a6be022fbd2de11fb625e329a4263f2ebd921be
dsindex <hazzling@gmail.com>
2018-10-01 14:26:41 +0900

    -

--- test7.py
+++ test7.py
@@ -3,9 +3,22 @@
 import tensorflow as tf
 import numpy as np
 
+d = tf.constant([ [ [0.5, 0.3, 0.1, -0.4, 0.1],
+                    [0.34, 0.2, 0.4, 0.6, 0.1], 
+                    [0.04, 0.3, 0.5, -0.9, 0.1],
+                    [0.0, 0.0, 0.0, 0.0, 0.0] ],
+                  [ [0.5, 0.3, 0.1, -0.4, 0.1],
+                    [0.34, 0.2, 0.4, 0.6, 0.1], 
+                    [0.0, 0.0, 0.0, 0.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0, 0.0] ],
+                  [ [0.5, 0.3, 0.1, -0.4, 0.1],
+                    [0.34, 0.2, 0.4, 0.6, 0.1], 
+                    [0.04, 0.3, 0.5, -0.9, 0.1],
+                    [0.0, 0.0, 0.0, 0.0, 0.0] ] ], dtype=tf.float32)
+                    
 x = tf.constant([ [1, 1, 1, 0],
                   [1, 1, 0, 0], 
-                  [1, 1, 1, 0] ] )
+                  [1, 1, 1, 0] ], dtype=tf.float32)
 
 y = tf.expand_dims(x, -1)
 dim = 5
@@ -16,3 +29,5 @@ with tf.Session() as sess:
 	sess.run(init_op)
 	print sess.run(y)
 	print sess.run(z)
+    # element-wise
+	print sess.run(d*z)

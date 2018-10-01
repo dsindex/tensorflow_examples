a8f2ca2fbebf5ae3d4d80f91e8aaac49e122cba2
dsindex <hazzling@gmail.com>
2018-10-01 14:56:15 +0900

    -

--- test7.py
+++ test7.py
@@ -24,14 +24,30 @@ y = tf.expand_dims(x, -1)
 dim = 5
 z = tf.tile(y, [1, 1, dim])
 
-bc = tf.constant([1.0, 0.5, 0.5, 0.4, 0.1], dtype=tf.float32)
+# y == bc1
+bc1 = tf.constant([ [ [1.0],
+                      [1.0],
+                      [1.0],
+                      [0.0] ],
+                    [ [1.0],
+                      [1.0],
+                      [0.0],
+                      [0.0] ],
+                    [ [1.0],
+                      [1.0],
+                      [1.0],
+                      [0.0] ] ], dtype=tf.float32)
+
+bc2 = tf.constant([1.0, 1.0, 1.0, 0.0, 0.0], dtype=tf.float32)
 
 init_op = tf.global_variables_initializer()
 with tf.Session() as sess:
 	sess.run(init_op)
-	print sess.run(y)
-	print sess.run(z)
+	print 'y', sess.run(y)
+	print 'z', sess.run(z)
     # element-wise
-	print sess.run(d*z)
-	# broadcasting
-	print sess.run(d*bc)
+	print 'd*z', sess.run(d*z)
+	# broadcasting 1
+	print 'd*y', sess.run(d*y)
+	# broadcasting 2
+	print 'd*bc2', sess.run(d*bc2)
